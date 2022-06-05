d38711b0748b9d66fc2926ffe3fd637724828e5e
Yvan Satyawan <y_satyawan@hotmail.com>
2022-06-05 16:35:59 +0200

    Ingredient canonization refactor

--- ingredient-canonization.py
+++ ingredient-canonization.py
@@ -10,11 +10,7 @@ from argparse import ArgumentParser
 import pandas as pd
 from gensim.models import KeyedVectors
 import gensim.downloader as api
-import numpy as np
-import string
-from tqdm import tqdm
-
-PUNCTUATION_TABLE = str.maketrans('', '', string.punctuation)
+from word_embedding import embed_ingredient_string
 
 
 def parse_args():
@@ -81,7 +77,7 @@ def ingredient_split(ingredient: str) -> Tuple[str, str]:
     word_qtys = "|".join(word_qtys)
 
     # Fractions also have to be taken into account
-    fraction_codes = "[\u00BC-\u00BE]|[\u2150-\u215E]|\d/\d"
+    fraction_codes = r"[\u00BC-\u00BE]|[\u2150-\u215E]|\d/\d"
 
     # Structure used for ingredients
     # This is a multiline comment just so I can visualize what kind of regex I
@@ -112,103 +108,29 @@ def ingredient_split(ingredient: str) -> Tuple[str, str]:
     return amount, ingredient
 
 
-def match_canonical_name(ingredient: str,
-                         model: KeyedVectors,
-                         name_vecs: KeyedVectors) -> str or None:
-    """Matches the closest canonical name.
-
-    Matches the given ingredient to the closest name in the names dataframe
-    using cosine similarity. Uses a threshold of 0.8 as the cutoff below which
-    no canonical name is returned.
-
-    Matching is done by the GloVe embedding of canonical ingredient names.
-    Names which are multi-word have their embedding vectors averaged. Once a
-    GloVe embedding has been calculated, the cosine similarity between all
-    ingredients are calculated and the maximum, if it has a value above 0.8, is
-    used as the canonical name.
-
-    We use the pretrained model glove-wiki-gigaword-300.
-
-    References:
-        "Automated Identification of Food Substitutes Using Knowledge Graph
-            Embeddings". Loesch et al.
-        "GloVe: Global Vectors for Word Representation". Pennington et al.
-        <https://github.com/RaRe-Technologies/gensim-data>
-
-    Returns:
-        The canonical name or None
-    """
-    # First split ingredient into a list of words, without punctuation
-    ing_list = ingredient.translate(PUNCTUATION_TABLE).split(' ')
-    # Calculate its average embedding
-    embedding = calculate_averaged_embedding(ing_list, model)
-    if embedding is not None:
-        # Get the most similar word
-        most_similar = name_vecs.similar_by_vector(embedding, topn=1)[0]
-        if most_similar[1] > 0.8:
-            return most_similar[0]
-    # If embedding was None or the similarity < 0.8,
-    return None
-
-
-def ingredient_list_canonization(ingredient_list: List[str],
-                                 model: KeyedVectors,
-                                 name_vecs: KeyedVectors) -> List[list]:
-    """Takes a list and makes the appropriate triple for each ingredient.
+def embed_ingredient_list(ingredient_list: List[str],
+                          model: KeyedVectors) -> List[dict]:
+    """Takes a list of ingredients and makes the appropriate dictionaries.
 
     Args:
-        ingredient_list: The list of ingredients to process
+        ingredient_list: The list of ingredients to process.
+        model: The model to use for the embedding.
 
     Returns:
-        A list of tuples where each tuple is shaped like
-            (original_ingredient, processed_amount, ingredient, canonical name)
+        A list of dictionaries with keys [original_name, amount,
+            ingredient_name, embedding]
     """
     split_ingredients = []
     for ing in ingredient_list:
         amount, ingredient_name = ingredient_split(ing)
-        canonical_name = match_canonical_name(ingredient_name, model, name_vecs)
-        split_ingredients.append([ing, amount, ingredient_name, canonical_name])
+        embedding = embed_ingredient_string(ingredient_name, model)
+        split_ingredients.append({
+            "original_name": ing,
+            "amount": amount,
+            "ingredient_name": ingredient_name,
+            "embedding": embedding})
 
     return split_ingredients
-
-
-def calculate_averaged_embedding(ingredient: List[str],
-                                 model: KeyedVectors) -> np.ndarray or None:
-    """Calculates the averaged word embedding of a list of words"""
-    vectors = []
-    for word in ingredient:
-        try:
-            vectors.append(model.get_vector(word.strip().lower()))
-        except KeyError:
-            pass
-    if len(vectors) > 0:
-        vectors = np.stack(vectors)
-        return np.mean(vectors, axis=0)
-    else:
-        return None
-
-
-def get_canonical_vectors(data_dir: Path, model: KeyedVectors):
-    vector_path = data_dir / "name_vectors.gz"
-    if not vector_path.exists():
-        # Open nutrition csv
-        names = pd.read_csv(data_dir / "nutrition.csv", usecols=['name'])
-
-        # Calculate the vector value of all name strings
-        print("Calculating vector values of all canonical ingredients")
-        keyed_vectors = KeyedVectors(300)
-        for name in names["name"].tolist():
-            vec = calculate_averaged_embedding(name.split(','), model)
-            if vec is not None:
-                keyed_vectors.add_vector(name, vec)
-        keyed_vectors.save(str(vector_path))
-        print("Named vectors calculated!")
-        return keyed_vectors
-    else:
-        print("Loading name vectors...")
-        keyed_vectors = KeyedVectors.load(str(vector_path))
-        print("Name vectors loaded!")
-        return keyed_vectors
 
 
 def main(data_dir: Path):
@@ -223,16 +145,12 @@ def main(data_dir: Path):
     model = api.load("glove-wiki-gigaword-300")
     print("Pretrained model loaded!")
 
-    # Get vectors of canonical ingredient names
-    name_vecs = get_canonical_vectors(data_dir, model)
-
     # For each recipe file
     for recipe_file in recipe_files:
         # Read the file
         df = pd.read_pickle(recipe_file)
         df["ingredients"] = df["ingredients"]\
-            .apply(lambda row: ingredient_list_canonization(row, model,
-                                                            name_vecs))
+            .apply(lambda row: embed_ingredient_list(row, model))
 
         df.to_pickle(recipe_file.with_name(recipe_file.stem + "_canonical.pkl"))
 
---
+++ word_embedding.py
@@ -0,0 +1,58 @@
+"""Word Embedding.
+
+Contains functions used for word embeddings.
+"""
+import string
+from typing import List
+
+import numpy as np
+from gensim.models import KeyedVectors
+
+PUNCTUATION_TABLE = str.maketrans('', '', string.punctuation)
+
+
+def embed_ingredient_string(ingredient_str: str,
+                            model: KeyedVectors) -> np.ndarray:
+    """Calculates the embedding of a given ingredient string.
+
+    Args:
+        ingredient_str: The ingredient string to be embedded.
+        model: The model with which to calculate the embedding.
+    """
+    # First split ingredient into a list of words, without punctuation
+    ing_list = ingredient_str.translate(PUNCTUATION_TABLE).split(' ')
+    # Calculate its average embedding
+    embedding = calculate_averaged_embedding(ing_list, model)
+    return embedding or None
+
+
+def calculate_averaged_embedding(ingredient: List[str],
+                                 model: KeyedVectors) -> np.ndarray or None:
+    """Calculates the averaged word embedding of a list of words.
+
+    Embedding is calculated by the GloVe embedding of canonical ingredient
+    names. Names which are multi-word have their embedding vectors averaged.
+
+    Matching canonical ingredient names are calculated at runtime by finding the
+    cosine similarity between an ingredient and all canonical ingredients and
+    the maximum, if it has a value above 0.8, is used as the canonical name.
+
+    We use the pretrained model glove-wiki-gigaword-300.
+
+    References:
+        "Automated Identification of Food Substitutes Using Knowledge Graph
+            Embeddings". Loesch et al.
+        "GloVe: Global Vectors for Word Representation". Pennington et al.
+        <https://github.com/RaRe-Technologies/gensim-data>
+    """
+    vectors = []
+    for word in ingredient:
+        try:
+            vectors.append(model.get_vector(word.strip().lower()))
+        except KeyError:
+            pass
+    if len(vectors) > 0:
+        vectors = np.stack(vectors)
+        return np.mean(vectors, axis=0)
+    else:
+        return None
